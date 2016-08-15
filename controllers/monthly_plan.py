b966c249a4122d18742a521dff0f45e4840bd2ea
gaurav sharma <gauravsharma.mvp@gmail.com>
2016-08-15 17:32:17 +0530

    15aug commit1

--- controllers/monthly_plan.py
+++ controllers/monthly_plan.py
@@ -7,4 +7,5 @@ class monthly_planHandler(tornado.web.RequestHandler):
 			id=self.get_secure_cookie('user')
 			result=yield db.users.find_one({'_id':ObjectId(id)})
 			self.render('data/monthly_plan.html',result=dict(user=result,loggedIn=bool(self.get_secure_cookie('user'))))
-		self.render('data/monthly_plan.html')
+		else:
+			self.render('data/monthly_plan.html',result=dict(loggedIn=bool(self.get_secure_cookie('user'))))
